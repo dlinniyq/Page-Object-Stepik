28c46f3852352f30e13906d88118628df8c290ce
dlinniyq <nikita.bulgakoff898@gmail.com>
2024-08-30 17:01:22 +0300

    final projet ( PAGE OBJECT half of 4.2)

---
+++ pages/base_page.py
@@ -0,0 +1,19 @@
+#Смысл этого класса - открыть страницу:
+# browser берется из conftest.py, url берется из test_main_page
+from selenium.common.exceptions import NoSuchElementException
+class BasePage():
+    def __init__(self, browser, url, timeout = 10):
+        self.browser = browser
+        self.url = url
+        self.browser.implicitly_wait(timeout)
+
+    def open(self):
+        self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:    #Проверка наличия исключения NoSuchElementException:
+            # если исключение именно такое, то False, иначе True
+            return False
+        return True
